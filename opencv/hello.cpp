f1e1ecb4bfd1edc6b9e88cba55125da730f3f6cd
mkh48v <mkh48v@snu.ac.kr>
2014-03-31 00:11:26 +0900

    edge detection added

--- opencv/hello.cpp
+++ opencv/hello.cpp
@@ -1,6 +1,73 @@
 #include <iostream>
 #include <opencv/cv.h>
 #include <opencv2/highgui/highgui.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace cv;
+
+/// Global variables
+
+Mat src, src_gray;
+Mat dst, detected_edges;
+
+int edgeThresh = 1;
+int lowThreshold;
+int const max_lowThreshold = 100;
+int ratio = 3;
+int kernel_size = 3;
+char* window_name = "Edge Map";
+
+/**
+ * @function CannyThreshold
+ * @brief Trackbar callback - Canny thresholds input with a ratio 1:3
+ */
+void CannyThreshold(int, void*)
+{
+  /// Reduce noise with a kernel 3x3
+  blur( src_gray, detected_edges, Size(3,3) );
+
+  /// Canny detector
+  Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size );
+
+  /// Using Canny's output as a mask, we display our result
+  dst = Scalar::all(0);
+
+  src.copyTo( dst, detected_edges);
+  imshow( window_name, dst );
+ }
+
+void face_recognition()
+{
+
+}
+
+void edge_detection()
+{
+	/// Load an image
+	src = imread("hello_world.jpg");
+
+	if( !src.data )
+	{
+		return;
+	}
+
+	/// Create a matrix of the same type and size as src (for dst)
+	dst.create( src.size(), src.type() );
+
+	/// Convert the image to grayscale
+	cvtColor( src, src_gray, CV_BGR2GRAY );
+
+	/// Create a window
+	namedWindow( window_name, CV_WINDOW_AUTOSIZE );
+
+	/// Create a Trackbar for user to enter threshold
+	createTrackbar( "Min Threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold );
+
+	/// Show the image
+	CannyThreshold(0, 0);
+}
 
 int main()
 {
@@ -24,9 +91,15 @@ int main()
 		return -2;
 	}
 
+	//edge detection 먼저
+	edge_detection();
+
+
+	//이 아래는 얼굴 인식
+	
 	//이미지를 로드
 	IplImage *frame = cvLoadImage("hello_world.jpg",CV_LOAD_IMAGE_COLOR);
-
+	
 	//얼굴 검출
 	CvSeq *faces = cvHaarDetectObjects(frame, cascade, storage, 1.4, 1, 0);
 
