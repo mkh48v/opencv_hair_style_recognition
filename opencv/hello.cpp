3d8b4787bb1ae642d4c4f523d1205a7c17d9df6d
mkh48v <mkh48v@snu.ac.kr>
2014-05-13 20:03:33 +0900

    algorithm changed

--- opencv/hello.cpp
+++ opencv/hello.cpp
@@ -17,8 +17,6 @@ int kernel_size = 3;
 
 int eye_upper_bound;
 
-cv::Mat grabbed_face;
-
 enum front_hair_style
 {
 	no_front_hair,
@@ -34,12 +32,16 @@ enum side_hair_style
 
 void CannyThreshold(int, void*)
 {
-  /// Reduce noise with a kernel 3x3
-  blur( src_gray, detected_edges, Size(3,3) );
+	/// Reduce noise with a kernel 3x3
+	blur( src_gray, detected_edges, Size(3,3) );
 
-  int threshold_value = 60;
-  Canny( detected_edges, detected_edges, threshold_value, threshold_value*ratio, kernel_size );
- }
+	int threshold_value = 50;
+	Canny( detected_edges, detected_edges, threshold_value, threshold_value*ratio, kernel_size );
+
+	cv::namedWindow("Result");
+	cv::imshow("Result", detected_edges);
+
+}
 
 void edge_detection()
 {
@@ -53,75 +55,56 @@ void edge_detection()
 	CannyThreshold(0, 0);
 }
 
-void show_detected_edges() 
+
+
+int get_left_front_hair_lower_bound(int head_upper_bound, int left_eye_right_bound) 
 {
-	//이쪽에서 아마 result 가 흰색이고 detected_edges에서도 흰색인 부분을 찾는다면 두 이미지를 합성하여 머리 경계를 찾아낼수 있을 것이다
-	for(int i = 0; i < detected_edges.rows; i++)
+	int front_left_hair_lower_bound = head_upper_bound + (4*(eye_upper_bound-head_upper_bound)/5.0);
+
+	while(front_left_hair_lower_bound > head_upper_bound + (eye_upper_bound-head_upper_bound)/4.0)
 	{
-		int j=0;
-		while(j < detected_edges.cols)
-		{
-			if(grabbed_face.at<uchar>(i,j) != 255 && detected_edges.at<uchar>(i,j) == 255)
-				grabbed_face.at<uchar>(i,j) = 255;
-
-			j++;
-		}
-	}
-	cv::namedWindow("Result");
-	cv::imshow("Result", grabbed_face);
-}
-
-void grabcut_face(int x, int y, int width, int height)
-{
-	cv::Mat image= cv::imread("hello_world.jpg");
-
-	// 입력 영상에 대한 부분 전경/배경 레이블을 지정하는 방법
-	// 전경 객체 내부를 포함하는 내부 직사각형을 정의
-	cv::Rect rectangle(x, y, width, height);
-	
-	cv::Mat bgModel, fgModel; // 모델 (초기 사용)
-	cv::grabCut (image,    // 입력 이미지
-		grabbed_face,    // 분할 결과
-		rectangle,   // 전경을 포함하는 직사각형
-		bgModel, fgModel, // 모델
-		1,     // 반복 횟수
-		cv::GC_INIT_WITH_RECT); // 직사각형 사용
-
-	// cv::GC_PR_FGD 전경에 속할 수도 있는 화소(직사각형 내부의 화소 초기값)
-	// cv::GC_PR_FGD와 동일한 값을 갖는 화소를 추출해 분할한 이진 영상을 얻음
-	cv::compare(grabbed_face, cv::GC_PR_FGD, grabbed_face, cv::CMP_EQ);
-	// 전경일 가능성이 있는 화소를 마크한 것을 가져오기
-	cv::Mat foreground(image.size(), CV_8UC3, cv::Scalar(255, 255, 255));
-	// 결과 이미지 생성
-	image.copyTo(foreground, grabbed_face);
-	// 배경 화소는 복사되지 않음
-}
-
-int get_front_hair_lower_bound() 
-{
-	int front_hair_lower_bound = 0;
-	for(int i = 0; i < grabbed_face.rows; i++)
-	{
-		int j=0;
-		while(j < grabbed_face.cols)
-		{
-			if(grabbed_face.at<uchar>(i,j) == 255)
-			{
-				front_hair_lower_bound=i;
-				break;
-			}
-			else
-			{
-				j++;
-			}
-		}
-		if(j != grabbed_face.cols)
+		if(detected_edges.at<uchar>( front_left_hair_lower_bound, left_eye_right_bound ) == 255)
 		{
 			break;
 		}
+		else
+		{
+			front_left_hair_lower_bound--;
+		}
 	}
-	return front_hair_lower_bound;
+
+	if(front_left_hair_lower_bound <=  head_upper_bound + (eye_upper_bound-head_upper_bound)/4.0)
+	{
+		//앞머리가 긴거임(여기까지 오도록 경계가 높을리가 없음)
+		front_left_hair_lower_bound = head_upper_bound + (4*(eye_upper_bound-head_upper_bound)/5.0);
+	}
+	return front_left_hair_lower_bound;
 }
+int get_right_front_hair_lower_bound(int head_upper_bound, int right_eye_left_bound) 
+{
+	int front_right_hair_lower_bound = head_upper_bound + (13*(eye_upper_bound-head_upper_bound)/15.0);
+
+	while(front_right_hair_lower_bound > head_upper_bound + (eye_upper_bound-head_upper_bound)/4.0)
+	{
+		if(detected_edges.at<uchar>( front_right_hair_lower_bound, right_eye_left_bound ) == 255)
+		{
+			break;
+		}
+		else
+		{
+			front_right_hair_lower_bound--;
+		}
+	}
+
+	if(front_right_hair_lower_bound <=  head_upper_bound + (eye_upper_bound-head_upper_bound)/4.0)
+	{
+		//앞머리가 긴거임(여기까지 오도록 경계가 높을리가 없음)
+		front_right_hair_lower_bound = head_upper_bound + (13*(eye_upper_bound-head_upper_bound)/15.0);
+	}
+	return front_right_hair_lower_bound;
+}
+
+
 
 int get_head_upper_bound() 
 {
@@ -152,33 +135,24 @@ int get_head_upper_bound()
 	return hair_upper_boundary_row;
 }
 
-int get_chin_line_bound(int front_hair_lower_bound) 
+int get_chin_line_bound(CvRect* face_rect) 
 {
-	int chin_line_bound = front_hair_lower_bound + 1;
-	while(chin_line_bound < grabbed_face.rows)
+	int chin_line_bound = (face_rect->y + face_rect->height + 7);
+
+	while( chin_line_bound < (int)(face_rect->y + face_rect->height * (5/4.0)) )
 	{
-		int j=0;
-		while(j < grabbed_face.cols)
+		if(detected_edges.at<uchar>( chin_line_bound, (int)( face_rect->x + (face_rect->width/2.0) ) ) == 255)
 		{
-			if(grabbed_face.at<uchar>(chin_line_bound,j) == 255)
-			{
-				break;
-			}
-			else
-			{
-				j++;
-			}
-		}
-		if(j == grabbed_face.cols)
-		{
-			//여기가 턱 끝임
-			chin_line_bound -= 1;//까만색 줄 위의 마지막 흰색 부분으로 bound 값 바꿈
 			break;
 		}
 		else
 		{
 			chin_line_bound++;
 		}
+	}
+	if( chin_line_bound >= (int)(face_rect->y + face_rect->height * (5/4.0)) )
+	{
+		chin_line_bound = (face_rect->y + face_rect->height + 7);
 	}
 	return chin_line_bound;
 }
@@ -295,7 +269,7 @@ front_hair_style judge_front_hairstyle(int front_hair_lower_bound, int hair_uppe
 
 int main()
 {
-	const char *faceclassifer = "C:\\opencv\\sources\\data\\haarcascades\\haarcascade_frontalface_alt_tree.xml";
+	const char *faceclassifer = "haarcascade_frontalface_alt.xml";
 	CvHaarClassifierCascade* facecascade = 0;
 	facecascade = (CvHaarClassifierCascade*) cvLoad(faceclassifer, 0, 0, 0 );
 
@@ -312,72 +286,97 @@ int main()
 	//이 아래는 얼굴 인식
 	
 	//이미지를 로드
-	IplImage *frame = cvLoadImage("hello_world.jpg",CV_LOAD_IMAGE_COLOR);
+	IplImage *pic = cvLoadImage("hello_world.jpg",CV_LOAD_IMAGE_COLOR);
 	
 	//얼굴 검출
-	CvSeq *detected_face = cvHaarDetectObjects(frame, facecascade, storage, 1.4 , 1, 0);
+	CvSeq *detected_face = cvHaarDetectObjects(pic, facecascade, storage, 1.4 , 1, 0);
 
 	//검출된 얼굴 Rectangle 받아오기(얼굴은 하나만 입력되고 인식됐다고 가정)
 	CvRect *face_rect = 0;
 	face_rect = (CvRect*) cvGetSeqElem(detected_face, 0);
 
-
 	//얼굴 이미지에서 눈을 찾아야 할 것 같음
 	cv::Mat face_mat= cv::imread("hello_world.jpg");
-
 	// Transform it into the C++ cv::Mat format
 	cv::Mat image(face_mat); 
 
-	//얼굴의 왼쪽 위(제 2사분면)의 사각형을 만들어서 이미지로 변환한다.
-	CvRect quarter_of_face;
-	quarter_of_face.x = face_rect -> x;
-	quarter_of_face.y = face_rect -> y;
-	quarter_of_face.width = (face_rect -> width)*0.5;
-	quarter_of_face.height = (face_rect -> height)*0.5;
 
-	cv::Mat cropped_face = image(quarter_of_face);
+	CvRect left_quarter_of_face;
+	left_quarter_of_face.x = face_rect -> x;
+	left_quarter_of_face.y = face_rect -> y;
+	left_quarter_of_face.width = (face_rect -> width)*0.5;
+	left_quarter_of_face.height = (face_rect -> height)*0.5;
+
+	CvRect right_quarter_of_face;
+	right_quarter_of_face.x = face_rect -> x + (face_rect -> width)*0.5;
+	right_quarter_of_face.y = face_rect -> y;
+	right_quarter_of_face.width = (face_rect -> width)*0.5;
+	right_quarter_of_face.height = (face_rect -> height)*0.5;
+
+	cv::Mat cropped_left_face = image(left_quarter_of_face);
+	cv::Mat cropped_right_face = image(right_quarter_of_face);
 
 	//cropped_face를 IplImage로 변환해야 함
-	IplImage* face_iplimage = &IplImage(cropped_face);
-
+	IplImage* left_face_iplimage = &IplImage(cropped_left_face);
+	IplImage* right_face_iplimage = &IplImage(cropped_right_face);
 
 	//face_iplimage에서 눈깔을 찾는다
-	CvSeq *eye = cvHaarDetectObjects(face_iplimage, eyecascade, storage, 1.4 , 1, 0);
+	CvSeq* left_eye = cvHaarDetectObjects(left_face_iplimage, eyecascade, storage, 1.4 , 1, 0);
+	CvSeq* right_eye = cvHaarDetectObjects(right_face_iplimage, eyecascade, storage, 1.4 , 1, 0);
 
 	//검출된 눈깔 Rectangle 받아오기(눈알은 하나만 입력되고 인식됐다고 가정)
-	CvRect *eye_rect = 0;
-	eye_rect = (CvRect*) cvGetSeqElem(eye, 0);
+	CvRect *left_eye_rect = 0;
+	left_eye_rect = (CvRect*) cvGetSeqElem(left_eye, 0);
 
-	eye_upper_bound = (face_rect->x) + (eye_rect->x);
-	
-	grabcut_face(face_rect->x, face_rect->y + (0.1)*(face_rect->height), face_rect->width, face_rect->height);//머리 모양 찾기
-	
-	//앞머리 라인
-	int front_hair_lower_bound = get_front_hair_lower_bound();
+	CvRect *right_eye_rect = 0;
+	right_eye_rect = (CvRect*) cvGetSeqElem(right_eye, 0);
 
+
+	//eye_rect의 좌표를 기억해놓아서 앞머리의 상대적 길이를 판정할 때 도움을 받아야 함 
+	cvRectangle(pic, cvPoint( face_rect->x + left_eye_rect->x, (face_rect->y)+(left_eye_rect->y) ), cvPoint(face_rect->x + left_eye_rect->x+left_eye_rect->width, (face_rect->y)+(left_eye_rect->y)+left_eye_rect->height), cvScalar(0,255,0), 3, CV_AA, 0);
+
+	cvRectangle(pic, cvPoint(face_rect->x, face_rect->y), cvPoint(face_rect->x+face_rect->width, face_rect->y+face_rect->height), cvScalar(0,255,0), 3, CV_AA, 0);
+
+
+
+	eye_upper_bound = (face_rect->y) + (left_eye_rect->y);
+	
 	//윗머리 끝 라인
 	int hair_upper_boundary_row = get_head_upper_bound();
 
+	//앞머리 라인
+	int front_hair_lower_bound = get_left_front_hair_lower_bound(hair_upper_boundary_row, face_rect->x + left_eye_rect->x + left_eye_rect->width);
+
+
+	//회사에서 부수적으로 요구하는 정보
+	int front_hair_left_lower_bound = get_left_front_hair_lower_bound(hair_upper_boundary_row, face_rect->x + left_eye_rect->x + left_eye_rect->width);
+	int front_hair_right_lower_bound = get_right_front_hair_lower_bound(hair_upper_boundary_row, face_rect->x + (int)(face_rect->width / 2.0) + right_eye_rect->x);
+
+
 	//턱 경계 찾아야 함
-	int chin_line_bound = get_chin_line_bound(front_hair_lower_bound);
+	int chin_line_bound = get_chin_line_bound(face_rect);
+
 
 
 	//옆머리 아랫쪽 경계를 찾아야 함
 	//왼쪽(당사자의 오른쪽) 옆머리의 두께를 위에서부터 내려가며 측정해나가며 0이 되는 순간 그곳의 row, column 좌표를 기록한다
-	int left_hair_lower_bound=hair_upper_boundary_row + (chin_line_bound - hair_upper_boundary_row)*(1.0/2.0) - 1;
+	int left_hair_lower_bound = face_rect->y + left_eye_rect->y + left_eye_rect->height;
+	int face_left_boundary=face_rect->x + left_eye_rect->x + (left_eye_rect->width/2.0);
 	while(left_hair_lower_bound < chin_line_bound)
 	{
-		//우선 result의 col 좌표를 찾아야함
-		int face_left_boundary=0;
-		while(face_left_boundary < grabbed_face.cols)
+		if(detected_edges.at<uchar>(left_hair_lower_bound,face_left_boundary) == 255)
 		{
-			if(grabbed_face.at<uchar>(left_hair_lower_bound,face_left_boundary) == 255)
+			face_left_boundary+=5;
+		}
+		while(face_left_boundary < face_rect->x)
+		{
+			if(detected_edges.at<uchar>(left_hair_lower_bound,face_left_boundary) == 255)
 			{
 				break;
 			}
 			else
 			{
-				face_left_boundary++;
+				face_left_boundary--;
 			}
 		}
 		//찾은 col 왼쪽 경계에서부터 detected_edges에 있는 옆 경계까지의 거리를 측정해야 함
@@ -405,15 +404,14 @@ int main()
 	}
 
 
+
 	side_hair_style detected_side_hair = judge_side_hairstyle(left_hair_lower_bound, chin_line_bound, hair_upper_boundary_row);
 
 	front_hair_style detected_front_hair = judge_front_hairstyle(front_hair_lower_bound, hair_upper_boundary_row);
 
 	show_recommended_hairstyle(detected_front_hair, detected_side_hair);
 
-	show_detected_edges();
-
-	cvShowImage("haar example (exit = esc)",frame);
+	cvShowImage("haar example (exit = esc)",pic);
 	cvWaitKey(0);
 
 	cvDestroyWindow("haar example (exit = esc)");
